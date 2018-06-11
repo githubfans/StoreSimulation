567fbb52daabdabbe9c77d26ae211ea99a0117c3
AJI <ajijogja@gmail.com>
2018-06-11 10:31:31 +0700

    Update gen1.py

--- gen1.py
+++ gen1.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
 	import hashlib
         
 	# choose 1000 Users/buyers random
-	select_query = "SELECT id from Users Where 1 order by RAND() LIMIT 1000"
+	select_query = "SELECT id, firstname UName from Users Where 1 order by RAND() LIMIT 1000"
         cursor = db.query(select_query)
         nt = 0
 	for (id) in cursor:
@@ -137,7 +137,7 @@ if __name__ == "__main__":
                 for w in range(num_of_buys):
 			
 			# choose 1 product random
-			random_product = db.query("select id, idSeller, stock from Products where 1 and stock>=1 order by RAND() limit 0,1")
+			random_product = db.query("select id, idSeller, title, stock, s.firstname SName from Products p Join Seller s on p.idSeller=s.id where 1 and stock>=1 order by RAND() limit 0,1")
                         
 			# get id
 			for rp in random_product:
@@ -147,6 +147,7 @@ if __name__ == "__main__":
 			try:
 				db.insert("INSERT INTO SessionOrders SET idProduct={0}, numItems={1}, idUser={2}, dateadd='{3}', SessCode='{4}'" . format(id_product, num_item_buy, id['id'], currdatetime, session_code))
 				db.insert("UPDATE Products SET stock=stock-{0} WHERE id={1}" . format(num_item_buy, id_product))
+				print('{0} buy {1} ({2} items)' . format(id['UName'], rp['title'], num_item_buy))
 				error = False
 			except:
 				print('error insert SessionOrders')
@@ -155,7 +156,7 @@ if __name__ == "__main__":
 			try:
 				nt += 1
 				db.insert("INSERT INTO Transaction SET idUser={0}, idSeller={1}, idSession='{2}', description='{3}', dateadd='{4}'" . format(id['id'], rp['idSeller'], session_code, '', currdatetime))
-				print('{0} Transaction : {1} successfully' . format(nt, session_code))
+				print('{0} Transaction : {1} successfully\n' . format(nt, session_code))
 			except:
 				# roll back
 				db.insert("DELETE FROM SessionOrders WHERE SessCode='{0}'" . format(session_code))
