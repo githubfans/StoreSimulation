394e88e666f1d0c61e3771a5dc2e5ce69e18a1e1
AJI <ajijogja@gmail.com>
2018-06-11 09:49:51 +0700

    Update gen1.py

--- gen1.py
+++ gen1.py
@@ -111,31 +111,47 @@ if __name__ == "__main__":
 	'''
 	
 	import hashlib
-        select_query = "SELECT id from Users Where 1 order by RAND() LIMIT 1000"
+        
+	# choose 1000 Users/buyers random
+	select_query = "SELECT id from Users Where 1 order by RAND() LIMIT 1000"
         cursor = db.query(select_query)
         for (id) in cursor:
 		currdatetime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		
+		# md5 for session code = receipt code
 		sessioncode = '{0}|{1}' . format(id['id'], strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 		m = hashlib.md5()
 		m.update(sessioncode)
 		session_code = m.hexdigest()
+		
+		# Users's buy 
 		num_of_buys = random.randint(1,10)
                 for w in range(num_of_buys):
+			
+			# choose 1 product random
 			random_product = db.query("select id, idSeller, stock from Products where 1 and stock>=1 order by RAND() limit 0,1")
-                        for rp in random_product:
+                        
+			# get id
+			for rp in random_product:
 				id_product = rp['id']
-				#print(id_product)
+				
 			num_item_buy = random.randint(1,rp['stock'])
-			queryin = "INSERT INTO SessionOrders SET idProduct={0}, numItems={1}, idUser={2}, dateadd='{3}', SessCode='{4}'" . format(id_product, num_item_buy, id['id'], currdatetime, session_code)
 			try:
-				#print(queryin)
-				db.insert(queryin)
+				db.insert("INSERT INTO SessionOrders SET idProduct={0}, numItems={1}, idUser={2}, dateadd='{3}', SessCode='{4}'" . format(id_product, num_item_buy, id['id'], currdatetime, session_code)
+)
 				db.insert("UPDATE Products SET stock=stock-{0} WHERE id={1}" . format(num_item_buy, id_product))
 				error = False
 			except:
 				print('error insert SessionOrders')
 				error = True
 		if error is False:
-			db.insert("INSERT INTO Transaction SET idUser={0}, idSeller={1}, idSession='{2}', description='{3}', dateadd='{4}'" . format(id['id'], rp['idSeller'], session_code, '', currdatetime))
+			try:
+				db.insert("INSERT INTO Transaction SET idUser={0}, idSeller={1}, idSession='{2}', description='{3}', dateadd='{4}'" . format(id['id'], rp['idSeller'], session_code, '', currdatetime))
+				print('Transaction : {0} successfully' . format(session_code))
+			except:
+				# roll back
+				db.insert("DELETE FROM SessionOrders WHERE SessCode='{0}'" . format(session_code))
+				db.insert("UPDATE Products SET stock=stock+{0} WHERE id={1}" . format(num_item_buy, id_product))
+				print('error transaction')
 		else:
 			print('error transaction')
