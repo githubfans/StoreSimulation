4f3d8baf7f601c48397a86a4b5371d5c16bdddf1
AJI <ajijogja@gmail.com>
2018-06-11 10:25:09 +0700

    Update gen1.py

--- gen1.py
+++ gen1.py
@@ -145,8 +145,7 @@ if __name__ == "__main__":
 				
 			num_item_buy = random.randint(1,rp['stock'])
 			try:
-				db.insert("INSERT INTO SessionOrders SET idProduct={0}, numItems={1}, idUser={2}, dateadd='{3}', SessCode='{4}'" . format(id_product, num_item_buy, id['id'], currdatetime, session_code)
-)
+				db.insert("INSERT INTO SessionOrders SET idProduct={0}, numItems={1}, idUser={2}, dateadd='{3}', SessCode='{4}'" . format(id_product, num_item_buy, id['id'], currdatetime, session_code))
 				db.insert("UPDATE Products SET stock=stock-{0} WHERE id={1}" . format(num_item_buy, id_product))
 				error = False
 			except:
