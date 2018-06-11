8da33da90e9d7eb16052059377b50c68ee562175
AJI <ajijogja@gmail.com>
2018-06-11 14:00:45 +0700

    Update GenProducts.py

--- GenProducts.py
+++ GenProducts.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     l = 0
     for (id) in cursor:
         l += 1
-        restock_or_generate = random.randint(0,2)
+        print('\n{0}-----' . format(l))
+        restock_or_generate = random.randint(1,4)
         if restock_or_generate is 1:
             currdatetime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
             for w in range(random.randint(10, 50)):
@@ -22,7 +23,7 @@ if __name__ == "__main__":
                 descr = gendesc(minitem=5, maxitem=10, minwords=5, maxwords=10, minchars=3, maxchars=7)
                 nstock = random.randint(5,100)
                 query = "INSERT INTO Products SET title = '{0}', idSeller = {1}, stock = {2}, description = '{3}', dateadd='{4}'" . format(title, id['id'], nstock, descr, currdatetime)
-                print('{0} {1}  Product created : {2} ({3} items)' . format(l, id['firstname'], title, nstock))
+                print('{0}  Product created : {1} ({2} items)' . format(id['firstname'], title, nstock))
                 db.insert(query)
         else:
             select_query = "SELECT id, title from Products Where 1 and stock<10 and idSeller={0} order by RAND() LIMIT 100" . format(id['id'])
@@ -31,6 +32,6 @@ if __name__ == "__main__":
                 try:
                     num_restock = random.randint(10,50)
                     db.query("UPDATE Products SET stock=stock+{0} where 1 and id={1}" . format(num_restock, Pid['id']))
-                    print('{0} Restock {1} + {2}' . format(l, Pid['title'], num_restock))
+                    print('Restock {0} + {1}' . format(l, Pid['title'], num_restock))
                 except:
-                    print('{0} Restock Fail {1} + {2}' . format(l, Pid['title'], num_restock))
+                    print('Restock Fail {0} + {1}' . format(l, Pid['title'], num_restock))
