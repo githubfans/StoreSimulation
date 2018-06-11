3d7e0a00d85850de69849f866d53a36a4baf3c91
AJI <ajijogja@gmail.com>
2018-06-11 13:13:02 +0700

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,28 @@
+class Database:
+
+    host = 'localhost'
+    user = 'aji'
+    password = '1234567!'
+    db = 'wikigen'
+
+    def __init__(self):
+        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        self.cursor = self.connection.cursor()
+
+    def insert(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+
+
+
+    def query(self, query):
+        cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
+        cursor.execute(query)
+
+        return cursor.fetchall()
+
+    def __del__(self):
+        self.connection.close()
