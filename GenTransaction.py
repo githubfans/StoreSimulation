56937063f6f41fa813688c013d98baf343a18322
AJI <ajijogja@gmail.com>
2018-06-12 10:12:36 +0700

    Update GenTransaction.py

--- GenTransaction.py
+++ GenTransaction.py
@@ -34,7 +34,7 @@ def Transaction(limit=100):
                 print('{0} - {1}' . format(id_product, num_item_buy))
                 qinsert = "INSERT INTO SessionOrders SET idProduct={0}, numItems={1}, idUser={2}, dateadd='{3}', SessCode='{4}'" . format(id_product, num_item_buy, id['id'], currdatetime, session_code)
                 print(qinsert)
-				qupdate = "UPDATE Products SET stock=stock-{0} WHERE id={1}" . format(num_item_buy, id_product)
+		qupdate = "UPDATE Products SET stock=stock-{0} WHERE id={1}" . format(num_item_buy, id_product)
                 print(qupdate)
                 try:
                     #db.insert(qinsert)
