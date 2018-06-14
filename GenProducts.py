5f608d432f990eadee5fe512757949066ac878d0
AJI <ajijogja@gmail.com>
2018-06-14 08:55:26 +0700

    Update GenProducts.py

--- GenProducts.py
+++ GenProducts.py
@@ -4,34 +4,43 @@ from time import gmtime, strftime
 import sys
 import random
 
-if __name__ == "__main__":
-
-    db = Database()
+def Transaction(limit=100):
+    if __name__ == "__main__":
+        db = Database()
         
-    # generate Product
-    select_query = "SELECT id, firstname from Seller Where 1 order by RAND() LIMIT 250"
-    cursor = db.query(select_query)
-    l = 0
-    for (id) in cursor:
-        l += 1
-        print('\n{0}-----' . format(l))
-        restock_or_generate = random.randint(1,4)
-        if restock_or_generate is 1:
-            currdatetime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            for w in range(random.randint(10, 25)):
-                title = genname(minwords=1,maxwords=4,minchars=3,maxchars=5, istitle=1)
-                descr = gendesc(minitem=5, maxitem=10, minwords=5, maxwords=10, minchars=3, maxchars=7)
-                nstock = random.randint(5,100)
-                query = "INSERT INTO Products SET title = '{0}', idSeller = {1}, stock = {2}, description = '{3}', dateadd='{4}'" . format(title, id['id'], nstock, descr, currdatetime)
-                print('{0}  Product created : {1} ({2} items)' . format(id['firstname'], title, nstock))
-                db.insert(query)
-        elif restock_or_generate is 2 or restock_or_generate is 3 or restock_or_generate is 4:
-            select_query = "SELECT id, title from Products Where 1 and stock<10 and idSeller={0} order by RAND() LIMIT 500" . format(id['id'])
-            cursor = db.query(select_query)
-            for (Pid) in cursor:
-                try:
-                    num_restock = random.randint(10,50)
-                    db.query("UPDATE Products SET stock=stock+{0} where 1 and id={1}" . format(num_restock, Pid['id']))
-                    print('Restock {0} + {1}' . format(Pid['title'], num_restock))
-                except:
-                    print('Restock Fail {0} + {1}' . format(Pid['title'], num_restock))
+        # generate Product
+        select_query = "SELECT id, firstname from Seller Where 1 order by RAND() LIMIT 250"
+        cursor = db.query(select_query)
+        l = 0
+        for (id) in cursor:
+            l += 1
+            print('\n{0}-----' . format(l))
+            restock_or_generate = random.randint(1,4)
+
+            if restock_or_generate is 1:
+                currdatetime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                for w in range(random.randint(10, 25)):
+                    title = genname(minwords=1,maxwords=4,minchars=3,maxchars=5, istitle=1)
+                    descr = gendesc(minitem=5, maxitem=10, minwords=5, maxwords=10, minchars=3, maxchars=7)
+                    nstock = random.randint(5,100)
+                    query = "INSERT INTO Products SET title = '{0}', idSeller = {1}, stock = {2}, description = '{3}', dateadd='{4}'" . format(title, id['id'], nstock, descr, currdatetime)
+                    print('{0}  Product created : {1} ({2} items)' . format(id['firstname'], title, nstock))
+                    db.insert(query)
+            
+            elif restock_or_generate is 2 or restock_or_generate is 3 or restock_or_generate is 4:
+                select_query = "SELECT id, title from Products Where 1 and stock<10 and idSeller={0} order by RAND() LIMIT 500" . format(id['id'])
+                cursor = db.query(select_query)
+                for (Pid) in cursor:
+                    try:
+                        num_restock = random.randint(10,50)
+                        db.query("UPDATE Products SET stock=stock+{0} where 1 and id={1}" . format(num_restock, Pid['id']))
+                        print('Restock {0} + {1}' . format(Pid['title'], num_restock))
+                    except:
+                        print('Restock Fail {0} + {1}' . format(Pid['title'], num_restock))
+
+                        
+try:
+    while True:
+        Transaction(limit=1)
+except KeyboardInterrupt:
+    break # do cleanup here
