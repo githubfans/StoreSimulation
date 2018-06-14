6849b2dc429248b8e54846766a27fa99c59a6fc2
AJI <ajijogja@gmail.com>
2018-06-14 14:54:41 +0700

    Update GenTransaction.py

--- GenTransaction.py
+++ GenTransaction.py
@@ -25,7 +25,7 @@ def Transaction(limit=100):
             # Users's buy 
             num_of_products = random.randint(1,20)
             # choose n product random
-            random_product = db.query("select p.id idpro, idSeller, title, stock, s.firstname SName from Products p Join Seller s on p.idSeller=s.id where 1 and stock>=1 order by RAND() limit 0,{0}" . format(num_of_products))
+            random_product = db.query("select p.id idpro, idSeller, title, stock, s.firstname SName from Products p Join Seller s on p.idSeller=s.id where 1 and stock>=10 order by RAND() limit 0,{0}" . format(num_of_products))
             ntrx = 0
             num_item_buy = 0
             sum_num_item_buy = 0
