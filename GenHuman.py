6698a4e2fc4c46da3f8a23278018fb516e662e67
AJI <ajijogja@gmail.com>
2018-06-22 14:51:08 +0700

    Update GenHuman.py

--- GenHuman.py
+++ GenHuman.py
@@ -19,7 +19,7 @@ def GenHuman(limit_user=100, limit_seller=100):
             name_1 = genname(minwords=1,maxwords=1,minchars=3,maxchars=5)
             name_2 = genname(minwords=1,maxwords=2,minchars=3,maxchars=5)
             email  = '{0}@{1}.com' . format(name_1, genword(minchars=5,maxchars=10, istitle=0))
-            check_email = db.query("SELECT email FROM Users WHERE 1 AND email='{0}'" . format(email))
+            check_email = db.insert("SELECT COUNT(*) FROM Users WHERE 1 AND email='{0}'" . format(email))
             print('{0} >> {1}' . format(g, check_email))
             if check_email is None:
                 query = "INSERT INTO Users SET firstname = '"+str(name_1)+"', lastname = '"+str(name_2)+"', email = '"+str(email)+"', dateadd='"+currdatetime+"'"
