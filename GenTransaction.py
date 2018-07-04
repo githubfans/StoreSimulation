612191999ce9d993d7c203618688cbf0af7e75f3
AJI <ajijogja@gmail.com>
2018-07-04 09:58:20 +0700

    Update GenTransaction.py

--- GenTransaction.py
+++ GenTransaction.py
@@ -27,10 +27,10 @@ def Transaction(limit=1, minbuy_numpro=1, maxbuy_numpro=10, min_stock_can_sell=1
 
             # choose n product random
             # lock this product from other transaction
-            qupdate = "UPDATE Products SET in_use='{0}' WHERE 1 AND in_use='' AND stock>=1 ORDER BY RAND() LIMIT {1}" . format(session_code, num_of_products)
+            qupdate = "UPDATE Products SET in_use='{0}' WHERE 1 AND in_use='' AND stock>={1} ORDER BY RAND() LIMIT {2}" . format(session_code, min_stock_can_sell, num_of_products)
             db.insert(qupdate)
             
-            random_product = db.query("SELECT p.id idpro, idSeller, title, stock, s.firstname SName FROM Products p JOIN Seller s on p.idSeller=s.id WHERE 1 AND stock>={0} AND p.in_use='{1}' ORDER BY RAND()" . format(min_stock_can_sell, session_code))
+            random_product = db.query("SELECT p.id idpro, idSeller, title, stock, s.firstname SName FROM Products p JOIN Seller s on p.idSeller=s.id WHERE 1 p.in_use='{0}' ORDER BY RAND()" . format(session_code))
             
             #for w in random_product:
             #    id_product = w['idpro']
